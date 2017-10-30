64098eb033477138a60c90147e8046afad539202
erikyoung <erik.young@rmit.edu.vn>
2017-10-30 14:29:39 +0700

    added pathways for most of routes I'll need

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
  root 'works#index' 
  resources :works
+
+ get 'about', to: 'works#about'
+ get 'portfolio', to: 'works#portfolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
