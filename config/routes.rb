74eb2e445274c19c61eaf10d92f2615445d95cd1
erikyoung <erik.young@rmit.edu.vn>
2017-11-02 20:02:23 +0700

    tryign to get images displayed correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
- root 'works#index' 
+ root 'works#static' 
  resources :works
 
+ get 'static', to: 'works#static'
  get 'about', to: 'works#about'
- get 'portfolio', to: 'works#portfolio'
+ get 'portfolio', to: 'works#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
